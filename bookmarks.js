937a1ac6cefdee37ca30837280f66f86ea558e8a
starseeker <54729715+whlGitHub@users.noreply.github.com>
2021-06-11 17:24:49 +0800

    Add files via upload

---
+++ bookmarks.js
@@ -0,0 +1,51 @@
+// Note: having length != 4 will mess with layout based on how the site is styled
+const bookmarks = [
+  {
+    title: "Tool",
+    links: [
+      { name: "百度网盘", url: "https://pan.baidu.com/disk/home" },
+      { name: "GDrive", url: "https://drive.google.com/drive/u/0/my-drive" },
+      { name: "语雀", url: "https://www.yuque.com/dashboard" },
+    ],
+  },
+  {
+    title: "Git",
+    links: [
+      { name: "Gitee", url: "https://gitee.com/" },
+      { name: "GitHub", url: "https://github.com" },
+    ],
+  },
+  {
+    title: "Media",
+    links: [
+      { name: "Youtube", url: "https://youtube.com" },
+      { name: "ddrk", url: "https://ddrk.me/category/movie" },
+      { name: "bilibili", url: "https://www.bilibili.com" },
+    ],
+  },
+  {
+    title: "Social",
+    links: [
+      { name: "Twitter", url: "https://twitter.com" },
+      { name: "Telegram", url: "https://web.telegram.org/#/im" },
+      { name: "Gmail", url: "https://mail.google.com/mail/u/0/#inbox" },
+      { name: "QQmail", url: "https://mail.qq.com/" },
+    ],
+  },
+  {
+    title: "Others",
+    links: [
+      { name: "信息门户", url: "http://campus.zzuli.edu.cn/portal-pc/pc/qy/index.html" },
+      {
+        name: "教务处",
+        url: "http://jwc.zzuli.edu.cn/",
+      },
+      {
+        name: "MOOC",
+        url: "https://www.icourse163.org/",
+      },
+      { name: "阿里云控制台", url: "https://account.aliyun.com/login/login.htm?oauth_callback=https%3A%2F%2Fecs.console.aliyun.com%2F%3Fspm%3D5176.12818093.recommends.decs.75e316d0JRPp6v#/home" },
+    ],
+  },
+
+];
